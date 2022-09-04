f7d8607be857cd52e93e51e7c2dfaeca5562802c
huynhthiphuongtram <69827176+huynhthiphuongtram@users.noreply.github.com>
2022-09-04 11:45:53 +0700

    Add files via upload

---
+++ QuanLySieuThi18-DoVanMinhHung18/QLST.BLL/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using QLST.Common.BLL;
+using QLST.Common.Rsp;
+using QLST.DAL;
+using QLST.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLST.BLL
+{
+    public class ProductService: GenericSvc<ProductRep,Hanghoa>
+    {
+        private ProductRep productRep;
+        public ProductService()
+        {
+            productRep = new ProductRep();
+        }
+        public override SingleRsp Read(int id)
+        {
+            var res = new SingleRsp();
+            res.Data = _rep.Read(id);
+            return res;
+        }
+    }
+}
