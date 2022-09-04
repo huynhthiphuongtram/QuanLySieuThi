b145a51faa4f5df2849a6f6737822a185258e115
huynhthiphuongtram <69827176+huynhthiphuongtram@users.noreply.github.com>
2022-09-04 11:50:34 +0700

    Add files via upload

---
+++ QuanLySieuThi18-DoVanMinhHung18/QLST.DAL/Models/Khachhang.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace QLST.DAL.Models
+{
+    public partial class Khachhang
+    {
+        public int Idkhachhang { get; set; }
+        public string Tenkh { get; set; }
+        public string Ngaysinh { get; set; }
+    }
+}
---
+++ QuanLySieuThi18-DoVanMinhHung18/QLST.Web/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLST.BLL;
+using QLST.Common.Req;
+using QLST.Common.Rsp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLST.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private ProductService productService;
+        public ProductController()
+        {
+            productService = new ProductService();
+        }
+        [HttpPost("get-by-id")]
+        public IActionResult GetProductByID([FromBody] SimpleReq simpleReq)
+        {
+            var res = new SingleRsp();
+            res = productService.Read(simpleReq.Id);
+            return Ok(res);
+        }
+    }
+}
